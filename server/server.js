c414a334878f861bb968eb0925f3c71b8f6e4e3b
Benjamin Effress <126579023+Ben-Effress@users.noreply.github.com>
2024-12-14 10:55:18 -0500

    fixed data on insights page

--- server/server.js
+++ server/server.js
@@ -22,6 +22,9 @@ app.get('/contributions_by_state/:state', routes.contributions_by_state);
 app.get('/contributors_by_state/:state', routes.contributors_by_state);
 app.get('/employer_contributions', routes.employer_contributions);
 app.get('/occupation_contributions', routes.occupation_contributions);
+app.get('/all_employer_stats', routes.all_employer_stats);
+app.get('/filtered_employer_stats', routes.filtered_employer_stats);
+app.get('/all_occupation_stats', routes.all_occupation_stats);
 
 app.listen(config.server_port, () => {
   console.log(`Server running at http://${config.server_host}:${config.server_port}/`)
